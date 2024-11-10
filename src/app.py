0308713332ad906a570a25e0838f9becc64243f0
LexoBrunett <56777101+LexoBrunett@users.noreply.github.com>
2024-11-10 20:00:24 +0000

    model

--- src/app.py
+++ src/app.py
@@ -1,4 +1,114 @@
 from utils import db_connect
 engine = db_connect()
 
-# your code here
+df = pd.read_csv("../input/acea-water-prediction/Aquifer_Petrignano.csv")
+df.head()
+
+# Remove old rows
+df = df[df.Rainfall_Bastia_Umbra.notna()].reset_index(drop=True)
+# Remove not useful columns
+df = df.drop(['Depth_to_Groundwater_P24', 'Temperature_Petrignano'], axis=1)
+
+# Simplify column names
+df.columns = ['date', 'rainfall', 'depth_to_groundwater', 'temperature', 'drainage_volume', 'river_hydrometry']
+
+targets = ['depth_to_groundwater']
+features = [feature for feature in df.columns if feature not in targets]
+df.head()
+
+from datetime import datetime, date 
+
+df['date'] = pd.to_datetime(df['date'], format = '%d/%m/%Y')
+df.head().style.set_properties(subset=['date'], **{'background-color': 'dodgerblue'})
+
+# To complete the data, as naive method, we will use .ffill()
+f, ax = plt.subplots(nrows=5, ncols=1, figsize=(15, 25))
+
+for i, column in enumerate(df.drop('date', axis=1).columns):
+    sns.lineplot(x=df['date'], y=df[column].fillna(method='ffill'), ax=ax[i], color='dodgerblue')
+    ax[i].set_title('Feature: {}'.format(column), fontsize=14)
+    ax[i].set_ylabel(ylabel=column, fontsize=14)
+                      
+    ax[i].set_xlim([date(2009, 1, 1), date(2020, 6, 30)]) 
+    
+
+df = df.sort_values(by='date')
+
+# Check time intervals
+df['delta'] = df['date'] - df['date'].shift(1)
+
+df[['date', 'delta']].head()
+df = df.sort_values(by='date')
+
+# Check time intervals
+df['delta'] = df['date'] - df['date'].shift(1)
+
+df[['date', 'delta']].head()
+
+df['delta'].sum(), df['delta'].count()
+
+df = df.drop('delta', axis=1)
+df.isna().sum()
+
+f, ax = plt.subplots(nrows=2, ncols=1, figsize=(15, 15))
+
+old_hydrometry = df['river_hydrometry'].copy()
+df['river_hydrometry'] = df['river_hydrometry'].replace(0, np.nan)
+
+sns.lineplot(x=df['date'], y=old_hydrometry, ax=ax[0], color='darkorange', label='original')
+sns.lineplot(x=df['date'], y=df['river_hydrometry'].fillna(np.inf), ax=ax[0], color='dodgerblue', label='modified')
+ax[0].set_title('Feature: Hydrometry', fontsize=14)
+ax[0].set_ylabel(ylabel='Hydrometry', fontsize=14)
+ax[0].set_xlim([date(2009, 1, 1), date(2020, 6, 30)])
+
+old_drainage = df['drainage_volume'].copy()
+df['drainage_volume'] = df['drainage_volume'].replace(0, np.nan)
+
+sns.lineplot(x=df['date'], y=old_drainage, ax=ax[1], color='darkorange', label='original')
+sns.lineplot(x=df['date'], y=df['drainage_volume'].fillna(np.inf), ax=ax[1], color='dodgerblue', label='modified')
+ax[1].set_title('Feature: Drainage', fontsize=14)
+ax[1].set_ylabel(ylabel='Drainage', fontsize=14)
+ax[1].set_xlim([date(2009, 1, 1), date(2020, 6, 30)])
+
+f, ax = plt.subplots(nrows=1, ncols=1, figsize=(16,5))
+
+sns.heatmap(df.T.isna(), cmap='Blues')
+ax.set_title('Missing Values', fontsize=16)
+
+for tick in ax.yaxis.get_major_ticks():
+    tick.label.set_fontsize(14)
+plt.show()
+
+f, ax = plt.subplots(nrows=4, ncols=1, figsize=(15, 12))
+
+sns.lineplot(x=df['date'], y=df['drainage_volume'].fillna(0), ax=ax[0], color='darkorange', label = 'modified')
+sns.lineplot(x=df['date'], y=df['drainage_volume'].fillna(np.inf), ax=ax[0], color='dodgerblue', label = 'original')
+ax[0].set_title('Fill NaN with 0', fontsize=14)
+ax[0].set_ylabel(ylabel='Volume C10 Petrignano', fontsize=14)
+
+mean_drainage = df['drainage_volume'].mean()
+sns.lineplot(x=df['date'], y=df['drainage_volume'].fillna(mean_drainage), ax=ax[1], color='darkorange', label = 'modified')
+sns.lineplot(x=df['date'], y=df['drainage_volume'].fillna(np.inf), ax=ax[1], color='dodgerblue', label = 'original')
+ax[1].set_title(f'Fill NaN with Mean Value ({mean_drainage:.0f})', fontsize=14)
+ax[1].set_ylabel(ylabel='Volume C10 Petrignano', fontsize=14)
+
+sns.lineplot(x=df['date'], y=df['drainage_volume'].ffill(), ax=ax[2], color='darkorange', label = 'modified')
+sns.lineplot(x=df['date'], y=df['drainage_volume'].fillna(np.inf), ax=ax[2], color='dodgerblue', label = 'original')
+ax[2].set_title(f'FFill', fontsize=14)
+ax[2].set_ylabel(ylabel='Volume C10 Petrignano', fontsize=14)
+
+sns.lineplot(x=df['date'], y=df['drainage_volume'].interpolate(), ax=ax[3], color='darkorange', label = 'modified')
+sns.lineplot(x=df['date'], y=df['drainage_volume'].fillna(np.inf), ax=ax[3], color='dodgerblue', label = 'original')
+ax[3].set_title(f'Interpolate', fontsize=14)
+ax[3].set_ylabel(ylabel='Volume C10 Petrignano', fontsize=14)
+
+for i in range(4):
+    ax[i].set_xlim([date(2019, 5, 1), date(2019, 10, 1)])
+    
+plt.tight_layout()
+plt.show()
+
+df['drainage_volume'] = df['drainage_volume'].interpolate()
+df['river_hydrometry'] = df['river_hydrometry'].interpolate()
+df['depth_to_groundwater'] = df['depth_to_groundwater'].interpolate()
+
